3b80367de40eb75da65720369e711284a7ada294
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-03 13:07:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, fn) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    result.push(fn(array[i]));
+  }
+  return result;
+}
+
+function reduce(array, fn, str) {
+  let result = !!str ? str : array[0];
+  let i = !!str ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    result = fn(array[i], result);
+  }
+  return result;
+}
